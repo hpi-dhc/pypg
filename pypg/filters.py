fac349f786c5cf89614c589354b5f8895cb60973
Ariane Morassi Sasso <ariane.morassi-sasso@hpi.de>
2022-01-30 19:17:17 +0100

    Fixed error in movefy and improved comments

--- pypg/filters.py
+++ pypg/filters.py
@@ -8,20 +8,20 @@ PPG signals.
 
 Filters
 ----------
-    butterfy - Butterworth filter optimized for PPG signals.
-    chebyfy  - Chebyshev II filter optimized for PPG signals.
-    movefy   - Moving average filter.
+butterfy - Butterworth filter optimized for PPG signals.
+chebyfy  - Chebyshev II filter optimized for PPG signals.
+movefy   - Moving average filter.
 
 Returns
 ----------
-    filtered : pandas.Series or ndarray
-        Filtered PPG signal.
+filtered : pandas.Series or ndarray
+    Filtered PPG signal.
 
 References
 ----------
-    Liang Y, Elgendi M, Chen Z, Ward R. Analysis: An optimal filter for
-    short photoplethysmogram signals. Sci Data 2018;5:1–12.
-    https://doi.org/10.1038/sdata.2018.76.
+Liang, Y., Elgendi, M., Chen, Z., & Ward, R. (2018). An optimal filter for
+short photoplethysmogram signals. Scientific Data, 5(1), 180076.
+https://doi.org/10.1038/sdata.2018.76
 
 """
 
@@ -35,42 +35,42 @@ from .plots import simple_plot
 def butterfy(ppg, cutoff_frequencies, sampling_frequency, filter_type='bandpass',
              filter_order=4, verbose=False, figure_path=None):
     """
-    Butterworth filter with 'bandpass' as the filter_type and filter_order of
-    4. Described in the literature as the second best option for filtering PPG
+    Implements the Butterworth filter with 'bandpass' as the filter_type and filter_order of
+    4. Described by Liang et al. (2018) as the second best option for filtering PPG
     signals.
 
     Parameters
     ----------
-        ppg : pandas.Series, ndarray
-            The PPG signal.
-        cutoff_frequencies : int or list
-            The cutoff frequency(ies if bandpass) for the filter in Hz.
-        sampling_frequency : int
-            The sampling frequency of the signal in Hz.
-        filter_type : str, optional
-            Filter type (low, high, band - pass). Defaults to 'bandpass'.
-        filter_order : int, optional
-            Filter Order. Defaults to 4.
-        verbose : bool, optional
-            Plots the signal. Defaults to False.
-        figure_path : str, optional
-            The path for the PPG plot to be saved.
+    ppg : pandas.Series, ndarray
+        The PPG signal.
+    cutoff_frequencies : int or list
+        The cutoff frequency(ies if bandpass) for the filter in Hz.
+    sampling_frequency : int
+        The sampling frequency of the signal in Hz.
+    filter_type : str, optional
+        Filter type (lowpass, highpass, bandpass). Defaults to 'bandpass'.
+    filter_order : int, optional
+        Filter Order. Defaults to 4.
+    verbose : bool, optional
+        Plots the signal. Defaults to False.
+    figure_path : str, optional
+        The path for the PPG plot to be saved.
 
     Raises
     ----------
-        Exception
-            When PPG values are neither pandas.Series nor ndarray.
+    Exception
+        When PPG values are neither pandas.Series nor ndarray.
 
     Returns
     ----------
-        filtered: pandas.Series or ndarray
-            Filtered PPG signal.
+    filtered: pandas.Series or ndarray
+        Filtered PPG signal.
 
     References
     ----------
-        Liang Y, Elgendi M, Chen Z, Ward R. Analysis: An optimal filter for
-        short photoplethysmogram signals. Sci Data 2018;5:1–12.
-        https://doi.org/10.1038/sdata.2018.76.
+    Liang, Y., Elgendi, M., Chen, Z., & Ward, R. (2018). An optimal filter
+    for short photoplethysmogram signals. Scientific Data, 5(1), 180076.
+    https://doi.org/10.1038/sdata.2018.76
     """
     if isinstance(ppg, pd.core.series.Series):
         signal_values = ppg.values
@@ -102,45 +102,45 @@ def butterfy(ppg, cutoff_frequencies, sampling_frequency, filter_type='bandpass'
 def chebyfy(ppg, cutoff_frequencies, sampling_frequency, filter_type='bandpass',
             filter_order=4, band_attenuation=20, verbose=False, figure_path=None):
     """
-    Chebyshev II filter with 'bandpass' as the filter_type, a filter_order of 4,
-    and a band_attenuation of 20 dB. Described in the literature as the best filter for
+    Imlements the Chebyshev II filter with 'bandpass' as the filter_type, a filter_order of 4,
+    and a band_attenuation of 20 dB. Described by Liang et al. (2018) as the best filter for
     PPG signals.
 
     Parameters
     ----------
-        ppg : pandas.Series or ndarray
-                The PPG signal.
-        cutoff_frequencies : int or list
-                The cutoff frequency(ies if bandpass) for the filter in Hz.
-        sampling_frequency : int
-                The sampling frequency of the signal in Hz.
-        filter_type : str, optional
-                Filter type (low, high, band - pass). Defaults to 'bandpass'.
-        filter_order : int, optional
-                Filter Order. Defaults to 4.
-        band_attenuation : int, optional
-                The attenuation required in the stop band.
-                In decibels (dB), as a positive number. Defaults to 20.
-        verbose : bool, optional
-                Plots the signal. Defaults to False.
-        figure_path : str, optional
-                The path for the PPG plot to be saved.
+    ppg : pandas.Series or ndarray
+            The PPG signal.
+    cutoff_frequencies : int or list
+            The cutoff frequency(ies if bandpass) for the filter in Hz.
+    sampling_frequency : int
+            The sampling frequency of the signal in Hz.
+    filter_type : str, optional
+            Filter type (lowpass, highpass, bandpass). Defaults to 'bandpass'.
+    filter_order : int, optional
+            Filter Order. Defaults to 4.
+    band_attenuation : int, optional
+            The attenuation required in the stop band.
+            In decibels (dB), as a positive number. Defaults to 20.
+    verbose : bool, optional
+            Plots the signal. Defaults to False.
+    figure_path : str, optional
+            The path for the PPG plot to be saved.
 
     Raises
     ----------
-        Exception
-            When PPG values are neither pandas.Series nor ndarray.
+    Exception
+        When PPG values are neither pandas.Series nor ndarray.
 
     Returns
     ----------
-        filtered: pandas.Series or ndarray
-        Filtered PPG signal.
+    filtered: pandas.Series or ndarray
+    Filtered PPG signal.
 
     References
     ----------
-        Liang Y, Elgendi M, Chen Z, Ward R. Analysis: An optimal filter for
-        short photoplethysmogram signals. Sci Data 2018;5:1–12.
-        https://doi.org/10.1038/sdata.2018.76.
+    Liang, Y., Elgendi, M., Chen, Z., & Ward, R. (2018). An optimal filter for
+    short photoplethysmogram signals. Scientific Data, 5(1), 180076.
+    https://doi.org/10.1038/sdata.2018.76
     """
     if isinstance(ppg, pd.core.series.Series):
         signal_values = ppg.values
@@ -171,32 +171,32 @@ def chebyfy(ppg, cutoff_frequencies, sampling_frequency, filter_type='bandpass',
 
 def movefy(ppg, size, verbose=False, figure_path=None):
     """
-    Moving average filter.
+    Implements the moving average filter.
 
     Parameters
     ----------
-        ppg : pandas.Series or ndarray
-            The PPG signal.
-        size : int
-            Size of the np.ones mask to be convoluted.
-        verbose : bool, optional
-            Plot the signal. Defaults to False, by default False.
-        figure_path : [type], optional
-            The path for the PPG plot to be saved, by default None.
+    ppg : pandas.Series or ndarray
+        The PPG signal.
+    size : int
+        Size of the np.ones mask to be convoluted.
+    verbose : bool, optional
+        Plot the signal. Defaults to False, by default False.
+    figure_path : [type], optional
+        The path for the PPG plot to be saved, by default None.
 
     Returns
     -------
-        filtered: pandas.Series or ndarray
-            Filtered PPG signal.
+    filtered: pandas.Series or ndarray
+        Filtered PPG signal.
 
     Raises
     ------
-        Exception
-            When PPG values are neither pandas.Series nor ndarray.
+    Exception
+        When PPG values are neither pandas.Series nor ndarray.
 
     References
     ----------
-        https://stackoverflow.com/questions/13728392/moving-average-or-running-mean/27681394#27681394
+    https://stackoverflow.com/questions/13728392/moving-average-or-running-mean/27681394#27681394
     """
     if isinstance(ppg, pd.core.series.Series):
         signal_values = ppg.values
@@ -210,9 +210,10 @@ def movefy(ppg, size, verbose=False, figure_path=None):
     filtered = signal.convolve(signal_values, np.ones((size,))/size, mode='valid')
 
     if isinstance(ppg, pd.core.series.Series):
-        filtered = pd.DataFrame()
-        filtered.index = signal_index
         filtered = pd.Series(filtered)
+        padding = pd.Series(np.nan, index=range(len(signal_index) - len(filtered.index)))
+        filtered = filtered.append(padding,ignore_index=True)
+        filtered.index = signal_index
 
     if verbose:
         simple_plot(ppg=filtered, title='Filtered Signal',
