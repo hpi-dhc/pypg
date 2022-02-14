cb2eb310c4ba7d102a75487bdf58f0f2000dd34e
Florian Hermes <43293808+GoWithTheFlo95@users.noreply.github.com>
2022-02-14 18:27:44 +0000

    Add sdppg features extraction

--- pypg/features.py
+++ pypg/features.py
@@ -436,6 +436,318 @@ def statistical_cycle(ppg, sampling_frequency, factor=0.667, unit='ms', verbose=
     return cycle_features
 
 
+def sdppg(ppg, sampling_frequency, factor=0.6667, unit='ms', verbose=False):
+    """
+    Extracts features from the second derivative (APG) of the PPG cycles in a give PPG segment. Returns a pandas.DataFrame in
+    which each line contains the features for a given valid cycle (as described
+    by Li et al.) from the PPG segment given as input.
+
+    Parameters
+    ----------
+    ppg : pandas.Series, ndarray
+        The PPG signal.
+    sampling_frequency : int
+        The sampling frequency of the signal in Hz.
+    factor: float, optional
+        Number that is used to calculate the distance in relation to the
+        sampling_frequency, by default 0.667 (or 66.7%). The factor is based
+        on the paper by Elgendi et al. (2013).
+    unit : str, optional
+        The unit of the index, by default 'ms'.
+    verbose : bool, optional
+        If True, plots the features with and without outliers, by default False.
+
+    Raises
+    ----------
+    Exception
+        When PPG values are neither pandas.Series nor ndarray.
+
+    Returns
+    -------
+    segment_features : pd.DataFrame
+        A dataframe with the features from the second derivative (APG) of the PPG cycles in seconds for each valid
+        cycle in the PPG segment.
+    """
+    if isinstance(ppg, np.ndarray):
+        ppg = pd.Series(ppg)
+    elif not isinstance(ppg, pd.core.series.Series):
+        raise Exception('PPG values not accepted, enter a pandas.Series or ndarray.')
+
+    cycles = find_with_template(ppg, sampling_frequency, factor=0.6667, verbose=verbose)
+    # all signal peaks
+    all_peaks = signal.find_peaks(ppg.values, distance=factor*sampling_frequency)[0]
+    if verbose:
+        print('All Peaks: ', all_peaks)
+    if len(cycles) == 0:
+        return pd.DataFrame()
+
+    cur_index = 0
+    segment_features = pd.DataFrame()
+    for i, cycle in enumerate(cycles):
+        segment_features = segment_features.append(sdppg_cycle(cycle,
+                                         sampling_frequency, factor, unit, verbose=verbose),
+                                         ignore_index=True)
+
+    if verbose:
+        print('Cycle Features within Segment:')
+        print(segment_features)
+
+    # remove outliers
+    segment_features = _clean_segment_features_of_outliers(segment_features)
+
+    if verbose:
+        print('Cycle Features within Segment and no Outliers:')
+    return segment_features
+
+
+def sdppg_cycle(ppg, sampling_frequency, factor=0.667, unit='ms', verbose=False):
+    """
+    Extracts features from the second derivative (APG) for the PPG cycles. Returns a pandas.Series.
+
+    Parameters
+    ----------
+    ppg : pandas.Series, ndarray
+        The PPG signal.
+    sampling_frequency : int
+        The sampling frequency of the signal in Hz.
+    factor: float, optional
+        Number that is used to calculate the distance in relation to the
+        sampling_frequency, by default 0.667 (or 66.7%). The factor is based
+        on the paper by Elgendi et al. (2013).
+    unit : str, optional
+        The unit of the index, by default 'ms'.
+    verbose : bool, optional
+        If True, plots the features with and without outliers, by default False.
+
+    Raises
+    ----------
+    Exception
+        When PPG values are neither pandas.Series nor ndarray.
+
+    Returns
+    -------
+    cycle_features : pd.DataFrame
+        A dataframe with the features from the second derivative (APG) in seconds for the PPG cycle.
+    """
+
+    if isinstance(ppg, np.ndarray):
+        ppg = pd.Series(ppg)
+    elif not isinstance(ppg, pd.core.series.Series):
+        raise Exception('PPG values not accepted, enter a pandas.Series or ndarray.')
+
+    if not isinstance(ppg.index, pd.DatetimeIndex):
+        ppg.index = pd.to_datetime(ppg.index, unit=unit)
+
+    ppg = ppg.interpolate(method='time')
+    ppg = ppg - ppg.min()
+
+    peaks = signal.find_peaks(ppg.values, distance=factor*sampling_frequency)[0]
+    systolic_peak_index = peaks[0]
+
+    if verbose:
+        plt.figure()
+        plt.xlim((ppg.index.min(), ppg.index.max()))
+        plt.scatter(ppg.index[peaks], ppg[peaks])
+        plt.plot(ppg.index, ppg.values)
+
+    # second derviative of the PPG signal
+    sdppg_signal = np.gradient(np.gradient(ppg))
+
+    # features
+    # https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0076585
+    # https://www.researchgate.net/figure/Important-points-on-PPG-VPG-and-AVG-The-first-top-signal-is-an-epoch-of-PPG-The_fig4_339604879
+    # a is global maximum;
+    # b is the global minimum; (maybe get systolic_peak_index and get local minimum between a_index and systolic_peak_index)
+    # e and c are the local maximum;
+    # d is the local minimum in APG between e and c;
+    # D is local minimum in APG after e
+    # 
+    # and Gaurav Paper
+
+    # TODO: Can be improved!
+    # TODO: maybe add systolic peak index as reference for indices detection
+    
+    sdppg_signal_peaks, peak_dict = signal.find_peaks(sdppg_signal, height=0)
+    sdppg_signal_valleys, valley_dict = signal.find_peaks(-sdppg_signal, height=0)
+    
+    # a wave
+    if len(sdppg_signal_peaks) != 0:
+        
+        a_index = (sdppg_signal_peaks[np.argmax(peak_dict['peak_heights'])])
+        
+        ## subset signal + dictionary to only have left over indeces + magnitudes
+        old_len = len(sdppg_signal_peaks)
+        sdppg_signal_peaks = sdppg_signal_peaks[sdppg_signal_peaks > a_index[0]]
+        peak_dict['peak_heights'] = peak_dict['peak_heights'][old_len - len(sdppg_signal_peaks):]
+        
+        old_len = len(sdppg_signal_valleys)
+        sdppg_signal_valleys = sdppg_signal_valleys[sdppg_signal_valleys > a_index[0]]
+        valley_dict['peak_heights'] = valley_dict['peak_heights'][old_len - len(sdppg_signal_valleys):]
+    
+    # b wave
+    if len(sdppg_signal_valleys) != 0:
+        
+        b_index = (sdppg_signal_valleys[np.argmax(valley_dict['peak_heights'])])
+        
+        ## subset signal + dictionary to only have left over indeces + magnitudes
+        old_len = len(sdppg_signal_peaks)
+        sdppg_signal_peaks = sdppg_signal_peaks[sdppg_signal_peaks > b_index[0]]
+        peak_dict['peak_heights'] = peak_dict['peak_heights'][old_len - len(sdppg_signal_peaks):]
+        
+        old_len = len(sdppg_signal_valleys)
+        sdppg_signal_valleys = sdppg_signal_valleys[sdppg_signal_valleys > b_index[0]]
+        valley_dict['peak_heights'] = valley_dict['peak_heights'][old_len - len(sdppg_signal_valleys):]
+
+	# c and e wave
+    if len(sdppg_signal_peaks) >= 2:
+        
+        peaks = peak_dict['peak_heights'].argsort()[-2:]
+        
+        if len(peaks) == 2:
+            if peaks[0] < peaks[1]:
+                c_index = (sdppg_signal_peaks[peaks[0]])
+                e_index = (sdppg_signal_peaks[peaks[1]])
+            else:
+                e_index = (sdppg_signal_peaks[peaks[0]])
+                c_index = (sdppg_signal_peaks[peaks[1]])
+            
+            ## subset signal + dictionary to only have left over indeces + magnitudes
+            old_len = len(sdppg_signal_valleys)
+            sdppg_signal_valleys = sdppg_signal_valleys[sdppg_signal_valleys > c_index[0]]
+            valley_dict['peak_heights'] = valley_dict['peak_heights'][old_len - len(sdppg_signal_valleys):]
+    
+    # d wave
+    if len(sdppg_signal_valleys) >= 1 and (c_index or e_index):
+        
+        ## get only valleys between c and e
+        old_len = len(sdppg_signal_valleys)
+        sdppg_signal_valleys = sdppg_signal_valleys[sdppg_signal_valleys < e_index[0]]
+        
+        if old_len > len(sdppg_signal_valleys):
+            valley_dict['peak_heights'] = valley_dict['peak_heights'][:-(old_len - len(sdppg_signal_valleys))]
+        
+        if len(valley_dict['peak_heights']) != 0:
+            d_index = (sdppg_signal_valleys[valley_dict['peak_heights'].argmax()])
+    
+    ## SDPPG Signal values
+    ### a
+    if a_index:
+        a_val = (sdppg_signal[a_index])
+    
+    ### b
+    if b_index:
+        b_val = (sdppg_signal[b_index])
+    
+    ### c
+    if c_index:
+        c_val = (sdppg_signal[c_index])
+    
+    ### d
+    if d_index:
+        d_val = (sdppg_signal[d_index])
+    
+    ### e
+    if e_index:
+        e_val = (sdppg_signal[e_index])
+   
+    
+    if verbose:
+        plt.figure(figsize=(17,6))
+        plt.plot(sdppg_signal, label = 'SDPPG Signal')
+        plt.scatter(np.array([a_index, b_index, c_index, d_index, e_index]), np.array([a_val, b_val, c_val, d_val, e_val]), c='g', label='Representative Points')
+        plt.title('SDPPG Signal Wave Detection')
+        plt.xlabel('Time')
+        plt.ylabel('SDPPG')
+        plt.legend()
+    
+    
+    ## Time values of characteristics
+    ### Location of c with respect to time (AD)
+    if c_index:
+        AD = (c_index / sampling_frequency)
+    
+    ### Location of d with respect to time (AE)
+    if d_index:
+        AE = (d_index / sampling_frequency)
+    
+    ### Difference of peak and dicrotic notch with respect to time (CD)
+    if c_index and systolic_peak_index is not None:
+        CD = ((c_index - systolic_peak_index) / sampling_frequency)
+    
+    ### Difference of dicrotic notch and end of signal with respect to time (DF)
+    if c_index:
+        DF = ((len(sdppg_signal) - c_index)  / sampling_frequency)
+    
+    
+    ## PPG Signal values
+    ### Dicrotic notch (D')
+    if c_index:
+        D = (ppg[c_index])
+    
+    ### Diastolic point (E')
+        if d_index:
+            E = (ppg[d_index])
+    
+    
+    ## Ratios
+    ### First valley to the peak value of APG signal (b/a)
+    if b_val and a_val:
+        ratio_b_a = (b_val / a_val)
+    
+    ### Dicrotic notch to the peak value of APG signal (c/a)
+    if c_val and a_val:
+        ratio_c_a = (c_val / a_val)
+    
+    ### Diastolic point to the peak value of APG signal (d/a)
+    if d_val and a_val:
+        ratio_d_a = (d_val / a_val)
+    
+    ### e to the peak value of APG signal (e/a)
+    if e_val and a_val:
+        ratio_e_a = (e_val / a_val)
+    
+    ### Location of dicrotic notch with respect to the length of window (AD/AF)
+    if c_index:
+        ratio_AD_AF = (c_index / len(sdppg_signal))
+    
+    ### Difference of location of peak and dicrotic notch with respect to the length of window (CD/AF)
+    if c_index and systolic_peak_index is not None:
+        ratio_CD_AF = ((c_index - systolic_peak_index) / len(sdppg_signal))
+    
+    ### Location of diastolic point on PPG signal with respect to the length of window (AE/AF)
+    if d_index:
+        ratio_AE_AF = (d_index / len(sdppg_signal))
+    
+    ### Difference of dichrotic notch and end with  respect ot length of window (DF/AF)
+    if c_index:
+        ratio_DF_AF = ((len(sdppg_signal) - c_index) / len(sdppg_signal))
+
+    cycle_features = pd.DataFrame({
+                        'a_val': a_val,
+                        'b_val': b_val,
+                        'c_val': c_val,
+                        'd_val': d_val,
+                        'e_val': e_val,
+                        'AD': AD,
+                        'AE': AE,
+                        'CD': CD,
+                        'DF': DF,
+                        'D': D,
+                        'E': E,
+                        'ratio_b_a': ratio_b_a,
+                        'ratio_c_a': ratio_c_a,
+                        'ratio_d_a': ratio_d_a,
+                        'ratio_e_a': ratio_e_a,
+                        'ratio_AD_AF': ratio_AD_AF,
+                        'ratio_CD_AF': ratio_CD_AF,
+                        'ratio_AE_AF': ratio_AE_AF,
+                        'ratio_DF_AF': ratio_DF_AF,
+                        }, index=[0])
+
+    if verbose:
+        plt.show()
+    return cycle_features
+
 # takes a dataframe of calculated features and removes the outliers occurring due
 # to inaccuracies in the signal
 def _clean_segment_features_of_outliers(segment_df, treshold=0.8):
